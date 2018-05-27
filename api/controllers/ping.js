d0c93470129518654f963005507ef87bc8dd6f4d
root <root@shadow.localdomain>
2018-05-27 11:04:26 -0400

    Version 0.0.2

--- api/controllers/ping.js
+++ api/controllers/ping.js
@@ -5,7 +5,7 @@ exports.ping = (req, res, next) => {
   const ver = process.env.npm_package_version;
   const port = req.socket.localPort;
   res.status(200).json({
-    message: `pong (host: ${hostname}:${port}, ver: ${ver})`
+    message: `Aloha from (host: ${hostname}:${port}, ver: ${ver})`
   });
 };
 
